d5cf21189a754024ae0681460f590b039d73c124
joueyl <dzb08042@163.com>
2024-11-08 15:07:23 +0800

    更新版本号

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { Dice } from 'dice'
 
-import { Dice } from './three/index'
+import { onMounted, ref } from 'vue'
 
 const container = ref<HTMLDivElement>()
 onMounted(() => {
   const dice = new Dice({ element: container.value!, diceColor: 0xFF0000, shadowEnable: true, modlePath: '/dice/dice.glb' })
+  console.log(dice)
   dice.onLoaded(() => {
+    console.log('load')
     dice.renderer.setClearAlpha(0)
     // dice.renderer.setClearColor(0xFF0000)
   })
